169064f2c593aea7d4e321ef63be5a429ee766dc
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-05-08 18:11:29 +0200

    feat: project header setup

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,23 @@ import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 
 export default {
-  data() {
-    return {
-      store,
+    data() {
+      return {
+        store,
+      }
+    },
+  
+    components: {
+      AppHeader,
+      AppMain,
     }
-  }
 }
+
 </script>
 
 <template>
-  
+  <AppHeader />
+  <AppMain />
 </template>
 
 <style lang="scss">
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,58 @@
 <script>
-
+export default {
+    
+}
 </script>
 
 <template>
 <div>
-    <h1>Project Header</h1>
+    <div class="header">
+        <div class="container">
+            <h1>BOOLFLIX</h1>
+            <div class="search">
+                <label for="search">Trova un film o una serie</label>
+                <div>
+                    <input id="search" type="text" placeholder="Cerca...">
+                    <button>Trova</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
 <style lang="scss" scoped>
 @use "../style/partials/variable" as *;
 @use "../style/partials/mixin" as *;
+
+.header {
+    padding: 10px;
+
+    .container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 80%;
+        margin: 0 auto;
+
+        .search {
+            display: flex;
+            padding: 10px;
+            
+
+            label {
+                margin-right: 10px;
+                margin-top: 6px;
+            }
+
+            input {
+                padding: 5px 2px;
+            }
+
+            button {
+                padding: 5px 2px;
+            }
+        }
+    }
+}
 </style>
