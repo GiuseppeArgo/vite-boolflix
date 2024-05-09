ac13d7c6eb9f9d4efbbb3d50a230ec5b7d2b823f
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-05-09 16:29:55 +0200

    feat:  added flag (milestone 2 first half)

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,9 +1,34 @@
 @@ -0,0 +1,19 @@
 <script>
 export default {
+    
     props:{
         cardObj: Object,
     },
+
+
+    methods: {
+        showFlag(curFlag){
+            let imgUrl = ""
+            switch(curFlag){
+                case "en":
+                    imgUrl = "../src/assets/img/Inghilterra.png"
+                    break;
+                case "it":
+                    imgUrl = "../src/assets/img/Italia.png"
+                    break; 
+                case "fr":
+                    imgUrl = "../src/assets/img/Francia.png"
+                    break; 
+                case "es":
+                    imgUrl = "../src/assets/img/spagna.png"
+                    break;
+                default:
+                    imgUrl = "../src/assets/img/Sconosciuta.png"   
+            }
+            return imgUrl
+        }
+    }
 }
 </script>
 
@@ -12,7 +37,8 @@ export default {
         <div class="card">
             <p>Titolo: {{ this.cardObj.title }}</p>
             <p>Titolo originale: {{ this.cardObj.original_title }}</p>
-            <p>Lingua: {{ this.cardObj.original_language }}</p>
+            <p>Lingua:</p>
+            <img :src="showFlag(cardObj.original_language)" alt="" v-if="showFlag(cardObj.original_language) != 'Bandiera non disponibile'">
             <p>Voto: {{ this.cardObj.vote_average }}</p>
         </div>
     </div>
